4c52965b22ca9e9cba27435c427bc66d643ca766
amir-kedis <amir.kedis@gmail.com>
2024-05-29 16:40:18 +0300

    feat: handle camera movement with sphircal coorindates

--- 3dconway.c
+++ 3dconway.c
@@ -6,7 +6,7 @@ int main() {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "3D Conway's Game of Life");
 
-  Vector3 cameraSpherePosition = {10.0f, 45.0f, -45.0f}; // (r, θ, φ)
+  Vector3 cameraSpherePosition = {50.0f, +45.0f, +45.0f}; // (r, θ, φ)
   Camera3D camera = {
       .position = {0.0f, 0.0f, 0.0f},
       .target = {0.0f, 0.0f, 0.0f},
@@ -15,10 +15,17 @@ int main() {
       .projection = CAMERA_PERSPECTIVE,
   };
 
+  int grid[GRID_SIZE][GRID_SIZE][GRID_SIZE] = {0};
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     BeginMode3D(camera);
+    handleCameraInput(&camera, &cameraSpherePosition);
     moveCamera(&camera, &cameraSpherePosition);
+    ClearBackground(GetColor(BACKGROUND_COLOR));
+    randomizeGrid(grid);
+
+    DrawGrid(100, 1.0f);
 
     EndMode3D();
     EndDrawing();
@@ -30,7 +37,29 @@ int main() {
 }
 
 void moveCamera(Camera3D *cam, Vector3 *pos) {
-  cam->position.x = cosf(pos->z) * sinf(pos->y) * pos->x;
-  cam->position.y = sinf(pos->z) * sinf(pos->y) * pos->x;
-  cam->position.z = cosf(pos->y) * pos->x;
+  cam->position.x = pos->x * cosf(pos->y) * sinf(pos->z);
+  cam->position.y = pos->x * cosf(pos->z);
+  cam->position.z = pos->x * sinf(pos->z) * sinf(pos->y);
+}
+
+void randomizeGrid(int grid[GRID_SIZE][GRID_SIZE][GRID_SIZE]) {
+  for (int x = 0; x < GRID_SIZE; x++)
+    for (int y = 0; y < GRID_SIZE; y++)
+      for (int z = 0; z < GRID_SIZE; z++)
+        grid[x][y][z] = GetRandomValue(0, 1);
+}
+
+void handleCameraInput(Camera3D *cam, Vector3 *pos) {
+  if (IsKeyDown(KEY_S))
+    pos->x += 5.1f * GetFrameTime();
+  if (IsKeyDown(KEY_W))
+    pos->x -= 5.1f * GetFrameTime();
+  if (IsKeyDown(KEY_D))
+    pos->y -= 0.1f * GetFrameTime();
+  if (IsKeyDown(KEY_A))
+    pos->y += 0.1f * GetFrameTime();
+  if (IsKeyDown(KEY_Q))
+    pos->z -= 0.1f * GetFrameTime();
+  if (IsKeyDown(KEY_E))
+    pos->z += 0.1f * GetFrameTime();
 }
--- 3dconway.h
+++ 3dconway.h
@@ -14,10 +14,17 @@
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 #define SCREEN_TITLE "3D Conway's Game of Life"
-#define BACKGROUND_COLOR 0x1E1E1E
+#define BACKGROUND_COLOR 0x222831FF
 
 // ==> CAMERA SETTINGS
 #define CAMERA_SPEED 1.0f
 
+// ==> GAME SETTINGS
+#define GRID_SIZE 10
+#define CELL_SIZE 5.0f
+#define CELL_COLOR 0xF2A365FF
+
 // ==> FUNCTION PROTOTYPES
 void moveCamera(Camera3D *cam, Vector3 *pos);
+void randomizeGrid(int grid[GRID_SIZE][GRID_SIZE][GRID_SIZE]);
+void handleCameraInput(Camera3D *cam, Vector3 *pos);
